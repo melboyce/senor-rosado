ff5eee4f4c37b7ce693dbfc34aaf7337779ef38d
Mel Boyce <syngin@me.com>
2017-04-23 21:36:22 +1000

    overhaul and now working

--- cmds/weather.go
+++ cmds/weather.go
@@ -2,13 +2,67 @@ package cmds
 
 import "fmt"
 import "os"
+import "time"
 
 import "encoding/json"
-import "io/ioutil"
 import "net/http"
 
-import "github.com/jmespath/go-jmespath"
 import "github.com/weirdtales/senor-rosado/slack"
+
+
+type darkSkyResp struct {
+    Timezone  string           `json:"timezone"`
+    Currently darkSkyCurrently `json:"currently"`
+}
+
+type darkSkyCurrently struct {
+    Summary     string  `json:"summary"`
+    Icon        string  `json:"icon"`
+    Temperature float64 `json:"temperature"`
+}
+
+type weatherLocation struct {
+    Results []struct {
+        FormattedAddress string `json:"formatted_address"`
+        Geometry struct {
+            Location struct {
+                Lat float64 `json:"lat"`
+                Lng float64 `json:"lng"`
+            } `json:"location"`
+        } `json:"geometry"`
+        Status string `json:"status"`
+    } `json:"results"`
+}
+
+type weatherInfo struct {
+    Latitude float64 `json:"latitude"`
+    Longitude float64 `json:"longitude"`
+    Timezone string `json:"timezone"`
+    Currently struct {
+        Time int64 `json:"time"`
+        Summary string `json:"summary"`
+        Icon string `json:"icon"`
+        Temperature float64 `json:"temperature"`
+        ApparentTemperature float64 `json:"apparentTemperature"`
+        DewPoint float64 `json:"dewPoint"`
+        Humidity float64 `json:"humidity"`
+        WindSpeed float64 `json:"windSpeed"`
+        Visibility float64 `json:"visibility"`
+        CloudCover float64 `json:"cloudCover"`
+        Pressure float64 `json:"pressure"`
+        Ozone float64 `json:"ozone"`
+    } `json:"currently"`
+    Daily struct {
+        Summary string `json:"summary"`
+    } `json:"daily"`
+    Hourly struct {
+        Summary string `json:"summary"`
+    } `json:"hourly"`
+
+    GoogleName string
+}
+
+var httpClient = &http.Client{Timeout: 10 * time.Second}
 
 
 // Weather reports the current forecast for an airport code
@@ -24,42 +78,63 @@ func Weather(m slack.Message, r *slack.Reply) error {
         return nil
     }
 
-    lat, lng, err := getLongLat(m.Subcommand)
+    // location query. not sure what to do about m.Tail...
+    q := m.Subcommand
+    loc, err := getLocation(q)
     if err != nil {
         return err
     }
-    fmt.Printf("lat=%f lng=%f\n\n", lat, lng)
+
+    fmt.Printf("\n%+v\n\n", loc)
+
+    w, err := getWeather(token, loc)
+    if err != nil {
+        return err
+    }
+    fmt.Printf("\n%+v\n\n", w)
+
+    if w.Timezone == "" {
+        r.Text = "hmm, something went wrong... (no data)"
+        return nil
+    }
+
+    report := "*%s* :point_right: %s %s, %.1f°C. %s %s"
+    t := time.Unix(w.Currently.Time, 0).Format("15:04")
+    r.Text = fmt.Sprintf(report, w.GoogleName, t, w.Currently.Summary, w.Currently.Temperature, w.Hourly.Summary, w.Daily.Summary)
+
     return nil
+
 }
 
-func getLongLat(q string) (lat float64, lng float64, err error) {
-    url := fmt.Sprintf("http://maps.googleapis.com/maps/api/geocode/json?address=%s&sensor=false", q)
-    r, err := http.Get(url)
-    if err != nil {
-        return
-    }
-    if r.StatusCode != 200 {
-        err = fmt.Errorf("Error: status code: %d", r.StatusCode)
-        return
-    }
+// returns a weatherLocation struct from googleapis.com/maps
+func getLocation(q string) (loc weatherLocation, err error) {
+    url := "http://maps.googleapis.com/maps/api/geocode/json?address=%s&sensor=false"
+    url = fmt.Sprintf(url, q) // TODO does `q` need to be sanitized?
+    err = getJson(url, &loc)
+    return
+}
 
-    body, err := ioutil.ReadAll(r.Body)
+// returns a weatherInfo struct from darksky
+func getWeather(token string, loc weatherLocation) (w weatherInfo, err error) {
+    w.GoogleName = loc.Results[0].FormattedAddress
+    url := "https://api.darksky.net/forecast/%s/%f,%f?units=si"
+    url = fmt.Sprintf(url, token, loc.Results[0].Geometry.Location.Lat, loc.Results[0].Geometry.Location.Lng)
+    fmt.Printf("\n%+v\n\n", url)
+    err = getJson(url, &w)
+    return
+}
+
+
+// ugh
+func getJson(url string, target interface{}) error {
+    r, err := httpClient.Get(url)
+    if err != nil {
+        return err
+    }
     defer r.Body.Close()
-    if err != nil {
-        return
+    if r.StatusCode != 200 {
+        return err
     }
 
-    var data interface{}
-    err = json.Unmarshal(body, &data)
-    if err != nil {
-        return
-    }
-
-    v, err := jmespath.Search("results[0].geometry.location", data)
-    if err != nil {
-        return
-    }
-    vals := v.(map[string]interface{})
-
-    return vals["lat"].(float64), vals["lng"].(float64), nil
+    return json.NewDecoder(r.Body).Decode(target)
 }
